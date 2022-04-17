26c732e8d80c1c23041140c4f726ed668fcc1775
yang2674 <steve@softwareforgood.com>
2022-04-16 20:47:44 -0500

    removed comments, fixed function call

--- dice-roll.js
+++ dice-roll.js
@@ -3,12 +3,8 @@ concepts: Math.random(), math.ceil, console.log */
 
 //dice function
 function dieRoll() {
-    //Math.random range 0 - 0.99999
     var n = Math.random();
-    /*I chose Math.ceil because it makes more sense that you can roll a 1-6 on a die.
-    Math.floor would also work but the result would be 0-5. If this method is chosen, you'd just have to add 1*/
-    console.log("You roll a die...It's a "+Math.ceil(n*6)+"!");
+    console.log("You roll the die...It's a "+Math.ceil(n*6)+"!");
 }
 
-//call function (aka roll the die)
-diceRoll();
+dieRoll();
