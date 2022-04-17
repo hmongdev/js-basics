899004332c3f67cb877a83e183a349b36cc50f5d
yang2674 <steve@softwareforgood.com>
2022-04-16 20:45:39 -0500

    add random generator

--- seasons.js
+++ seasons.js
@@ -1,16 +1,28 @@
-//concepts: else if statements, escaping
+//concepts: else if statements, escaping, switch statements
 //purpose: practice js basics
 
-let season = 'fall';
+let seasons = ["Fall", "Summer", "Winter", "Spring"];
 
-if (season === 'summer') {
-    console.log('It\'s hot and sunny outside!');
-} else if (season === 'fall') {
-    console.log('It\'s not too hot or cold. The leaves are changing color.');
-} else if (season === 'winter') {
-    console.log('It\'s cold outside! Wear something warm.');
-} else if (season === 'spring') {
-    console.log('There\'s a chance of rain! Bring an umbrella.');
-} else {
-    console.log('Invalid season');
+function changeSeasons() {
+    for (let i = 0; i < seasons.length; i++) {
+        let i = Math.floor(Math.random()*4);
+        if (seasons[i] === 'Fall') {
+            console.log('The season is Fall. It\'s not too hot or cold. The leaves are changing color.');
+            break;
+        } else if (seasons[i] === 'Summer') {
+            console.log('The season is Summer. It\'s hot and sunny outside!');
+            break;
+        } else if (seasons[i] === 'Fall') {
+            console.log('The season is Fall. It\'s not too hot or cold. The leaves are changing.');
+            break;
+        } else if (seasons[i] === 'Spring') {
+            console.log('The season is Spring. There\'s a chance of rain! Bring an umbrella.');
+            break;
+        } else {
+            console.log('Invalid season');
+            break;
+        }
+    }
 }
+
+changeSeasons();
