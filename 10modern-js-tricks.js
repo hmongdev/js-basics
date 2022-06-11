b4a24442c5a920cce00fa087aac745908c3fd194
Avery Yang <averyyang2674@gmail.com>
2022-06-10 23:02:01 -0500

    nullish coalescing

--- 10modern-js-tricks.js
+++ 10modern-js-tricks.js
@@ -1,5 +1,6 @@
 // https://betterprogramming.pub/10-modern-javascript-tricks-every-developer-should-use-377857311d79
 
+
 // 1. Conditionally Add Properties to Object
 //  ...(condition && {prop: value})
 
@@ -24,6 +25,7 @@ const greenAvenger = {
 
 console.log(greenAvenger);
 
+
 // 2. Check if a Property Exists in an Object
 // ('prop' in object)
 
@@ -32,6 +34,7 @@ console.log(greenAvenger);
 // console.log('age' in person); // returns false
 console.log(`Does \'Age\' exist in greenAvenger:`, 'Age' in greenAvenger); //true
 console.log(`Does \'Green\' exist in greenAvenger:`, 'green' in greenAvenger); //false
+
 
 // 3. Dynamic Property Names in Objects
 // let variable = whatever; [variable]: 'value'
@@ -44,6 +47,7 @@ console.log(`Does \'Green\' exist in greenAvenger:`, 'green' in greenAvenger); /
 // console.log(item); // { name: 'Biscuit', flavour: 'Chocolate' }
 console.log(greenAvenger); // [color: 'green']
 
+
 // 4. Object Destructuring With a Dynamic Key
 
 // const {prop: value} = obj
@@ -52,7 +56,9 @@ console.log(greenAvenger); // [color: 'green']
 // const person = { id: 1, name: 'John Doe' };
 // const { name: personName } = person;
 // console.log(personName); // returns 'John Doe'
-const { Age: number } = greenAvenger;
+const {
+  Age: number
+} = greenAvenger;
 console.log(`greenAvenger is`, number, `years old`); //49, if angry is false //13, if angry is true
 
 // Destructuring with a Dynamic Key:
@@ -64,3 +70,14 @@ console.log(`greenAvenger is`, number, `years old`); //49, if angry is false //1
 // const templateName = 'bye';
 // const { [templateName]: template } = templates;
 // console.log(template) // returns 'Good bye'
+
+
+// 5. Nullish Coalescing, ?? Operator
+// The ?? operator is useful when you want to check whether a variable is null or undefined. It returns the right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+
+const foo = null ? ? 'Hello';
+console.log(foo); // returns 'Hello'
+const bar = 'Not null' ? ? 'Hello';
+console.log(bar); // returns 'Not null'
+const baz = 0 ? ? 'Hello';
+console.log(baz); // returns 0
