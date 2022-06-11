1b47efdf3f7c34f9941fc0850ab527f3a711d019
Avery Yang <averyyang2674@gmail.com>
2022-06-10 22:45:06 -0500

    spread operator; property check

---
+++ 10modern-js-tricks.js
@@ -0,0 +1,32 @@
+// https://betterprogramming.pub/10-modern-javascript-tricks-every-developer-should-use-377857311d79
+
+// 1. Conditionally Add Properties to Object - Spread Operator
+const angry = true //false
+
+const greenAvenger = {
+  Age: 49,
+  Height: '5\'9',
+  Weight: '128lbs',
+  'Who Am I?': 'Bruce Banner, M.D., Ph.D.',
+  Green: false,
+
+  ...(angry && {
+    Age: 13,
+    Height: '7\'8\'',
+    Weight: '1,400 lbs',
+    'Who Am I?': 'The Hulk',
+    Green: true,
+    hulkSmash(){
+      console.log('HULK SMASHHHH!');
+    }
+  })
+};
+
+console.log(greenAvenger);
+
+// 2. Check if a Property Exists in an Object
+// const person = { name: 'John Doe', salary: 1000 };
+// console.log('salary' in person); // returns true
+// console.log('age' in person); // returns false
+console.log(`Does \'Age\' exist in greenAvenger:`,'Age' in greenAvenger); //true
+console.log(`Does \'Green\' exist in greenAvenger:`,'green' in greenAvenger); //false
